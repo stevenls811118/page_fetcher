18369e814dba9016d8059a45310a64622c848b31
Yifan Shi <stevenls1118@msn.com>
2022-10-22 00:09:13 -0600

    modified

---
+++ fetcher.js
@@ -0,0 +1,21 @@
+const request = require('request');
+const fs = require('fs');
+
+let arg = process.argv.slice(2);
+let URL = arg[0];
+let filePath = arg[1];
+
+const content = 'Some content!';
+
+request(URL, (error, response, body) => {
+
+  fs.writeFile(filePath, body, err => {
+    if (err) {
+      console.error(err);
+    } 
+    console.log(`Downloaded and saved ${body.length} bytes to ${filePath}`)
+    // file written successfully
+  });  
+});
+
+
